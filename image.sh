42a0c42ba5d8eb276189b3028938832f384baefe
loong576 <34957218+loong576@users.noreply.github.com>
2020-03-11 14:27:16 +0800

    Update image.sh

--- image.sh
+++ image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-url=registry.cn-hangzhou.aliyuncs.com/google_containers
-version=v1.14.2
+url=registry.cn-hangzhou.aliyuncs.com/loong576
+version=v1.16.4
 images=(`kubeadm config images list --kubernetes-version=$version|awk -F '/' '{print $2}'`)
 for imagename in ${images[@]} ; do
   docker pull $url/$imagename
